c02383a1d7dcd4fa20f07df55f977ca7cc636c4f
Horkrine <murloc23@icloud.com>
2019-03-04 23:13:32 +0000

    Imported the new CalculatorService as an implemented interface through .net core DI - also added an endpoint that currently just throws an exception

--- NU.StringCalculatorExercise.Web/Controllers/HomeController.cs
+++ NU.StringCalculatorExercise.Web/Controllers/HomeController.cs
@@ -4,15 +4,28 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using NU.StringCalculatorExercise.Logic.Contracts;
 using NU.StringCalculatorExercise.Web.Models;
 
 namespace NU.StringCalculatorExercise.Web.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ICalculatorService _calculatorService;
+
+        public HomeController(ICalculatorService calculatorService)
+        {
+            _calculatorService = calculatorService;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        public IActionResult Calculate([FromBody] CalculatorInputViewModel data)
+        {
+            throw new NotImplementedException();
         }
 
         public IActionResult Error()
