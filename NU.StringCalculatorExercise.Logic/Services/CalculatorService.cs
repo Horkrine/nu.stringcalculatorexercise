16a61790304b9428b44fc52e04cdddde19b1094d
Horkrine <murloc23@icloud.com>
2019-03-04 23:12:29 +0000

    Added the CalculatorService and it's contract (both are currently blank)

---
+++ NU.StringCalculatorExercise.Logic/Services/CalculatorService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NU.StringCalculatorExercise.Logic.Contracts;
+
+namespace NU.StringCalculatorExercise.Logic.Services
+{
+    public class CalculatorService : ICalculatorService
+    {
+    }
+}
