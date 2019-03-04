856ea7caa7f2e04d6a4834c15edffcb2758bfa7c
Horkrine <murloc23@icloud.com>
2019-03-04 23:12:06 +0000

    Added a new unit test class for the new (unadded) CalculatorService

---
+++ NU.StringCalculatorExercise.Tests/CalculatorServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using FluentAssertions;
+
+namespace NU.StringCalculatorExercise.Tests
+{
+    // Should_ExpectedBehaviour_When_Condition
+
+    public class CalculatorServiceTests
+    {
+        [Theory]
+        [InlineData("")]
+        public void Should_ThrowException_When_NotIntegers(string input)
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
