e823e05b61e635e83580ecbb5fbfbc344a23f0a6
Horkrine <murloc23@icloud.com>
2019-03-04 23:47:13 +0000

    Added the logic into the CalculatorService.Calculate method to get the values to total up. Now to add a front-end AJAX call to pass the data to the server and handle the response. All tests passed - builds and runs

--- NU.StringCalculatorExercise.Logic/Services/CalculatorService.cs
+++ NU.StringCalculatorExercise.Logic/Services/CalculatorService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NU.StringCalculatorExercise.Logic.Contracts;
+using System.Text.RegularExpressions;
 
 namespace NU.StringCalculatorExercise.Logic.Services
 {
@@ -11,7 +12,20 @@ namespace NU.StringCalculatorExercise.Logic.Services
     {
         public int Calculate(string input)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                return 0;
+            }
+
+            if (!Regex.IsMatch(input, "[0-9,]", RegexOptions.Compiled))
+            {
+                throw new Exception("Input must be a comma seperated list of integers");
+            }
+
+            var values = input.Split(',');  // Split the values out
+            var total = values.Sum(int.Parse);  // Parse each as an integer and then add them here
+
+            return total;
         }
     }
 }
